63e045c98f0b99d36cd75416cb0869c79f9351dc
Dmitry Kuznetsov <121979741+uh-h@users.noreply.github.com>
2024-03-04 17:38:19 +0300

    added docstring, removed unnecessary commas

--- async_sha256_file_hash.py
+++ async_sha256_file_hash.py
@@ -1,4 +1,6 @@
 import hashlib
+from pathlib import Path
+
 import aiofiles
 from logs.logger import logger
 
@@ -6,7 +8,16 @@ from logs.logger import logger
 chunk_size = 1024
 
 
-async def calculate_hash(file_path: str) -> str:
+async def calculate_hash(file_path: str | Path) -> str:
+    """
+    Asynchronously calculate the SHA256 hash of a file
+
+    Args:
+        file_path (str | Path): The path to the file
+
+    Returns:
+        str: A hexadecimal string representing the SHA256 hash digest of the file
+    """
     sha256 = hashlib.sha256()
     try:
         async with aiofiles.open(file_path, 'rb') as f:
--- file_event_handler.py
+++ file_event_handler.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import aiohttp
 from watchfiles import Change, awatch
@@ -12,19 +13,30 @@ from async_sha256_file_hash import calculate_hash
 
 
 class EventHandler:
-    def __init__(self, session: aiohttp.ClientSession, loop: asyncio.AbstractEventLoop, sync_paths: List[str]):
+    def __init__(self, session: aiohttp.ClientSession, loop: asyncio.AbstractEventLoop, sync_paths: List[str | Path]):
+        """
+        Class for handling file change events and sending them to the server
+        Args:
+            session (aiohttp.ClientSession): An aiohttp.ClientSession instance for making requests
+            loop (asyncio.AbstractEventLoop): The asyncio event loop where the class is running
+            sync_paths (List[str | Path]): List of paths to be watched for file changes
+        """
         self.session = session
         self.loop = loop
         self.sync_paths = sync_paths
 
-    async def start(self):
+    async def start(self) -> None:
+        """
+        Asynchronously starts watching the file changes in the specified sync_paths.
+        When a change is detected triggers a corresponding request to the server
+        """
         async for changes in awatch(*self.sync_paths):
             # unpack events
             file_events: List[List[Change | str]] = []
             for change, path in changes:
                 file_events.append([change, path])
 
-            # File was renamed. It creates 2 events: Change.deleted (3) and Change.added (1)
+            # file was renamed. It creates 2 events: Change.deleted (3) and Change.added (1)
             if len(file_events) > 1:
                 # Events can be in any order
                 if file_events[0][0] == 3:
@@ -37,11 +49,10 @@ class EventHandler:
                 old_rel_path = get_relative_path(self.sync_paths, old_path)
                 new_rel_path = get_relative_path(self.sync_paths, new_path)
 
-                await requests.file_name_update(self.session, old_path, old_rel_path, new_path, new_rel_path)
-                # asyncio.run_coroutine_threadsafe(
-                #     requests.file_name_update(self.session, old_name, new_name),
-                #     loop=self.loop
-                # )
+                asyncio.run_coroutine_threadsafe(
+                    requests.file_name_update(self.session, old_path, old_rel_path, new_path, new_rel_path),
+                    loop=self.loop
+                )
 
             event, file_path = file_events[0]
 
@@ -50,26 +61,34 @@ class EventHandler:
                 rel_path = get_relative_path(self.sync_paths, file_path)
 
                 file_hash = await calculate_hash(file_path)
-                await requests.file_hash_update(self.session, rel_path, file_path, file_hash)
-                # asyncio.run_coroutine_threadsafe(
-                #     requests.file_hash_update(self.session, rel_path, file_path, file_hash),
-                #     loop=self.loop
-                # )
+                asyncio.run_coroutine_threadsafe(
+                    requests.file_hash_update(self.session, rel_path, file_path, file_hash),
+                    loop=self.loop
+                )
             # file was deleted
             elif event == 3:
                 # send delete_file request
                 relative_path = get_relative_path(self.sync_paths, file_path)
-                await requests.delete_file(self.session, file_path, relative_path)
-                # asyncio.run_coroutine_threadsafe(
-                #     requests.delete_file(self.session, file_path, relative_path),
-                #     loop=self.loop
-                # )
+                asyncio.run_coroutine_threadsafe(
+                    requests.delete_file(self.session, file_path, relative_path),
+                    loop=self.loop
+                )
             else:
                 logger.error(f"Unexpected event: {changes}")
 
 
-def get_relative_path(directories: List[str], file_path: str) -> str | None:
-    # return path form watching directory to the file or None
+def get_relative_path(directories: List[str | Path], file_path: str | Path) -> str | None:
+    """
+    Look for a relative path in given List of directories and return the relative path of the file within the directory
+    or None if not found
+
+    Args:
+        directories (List[str]): List of directories to look for file
+        file_path (str): The path of the file
+
+    Return:
+        str | None: The relative path of the file within the directory
+    """
     for path in directories:
         path = os.path.dirname(path)
         if file_path.startswith(path):
