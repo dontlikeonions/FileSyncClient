85abeb7c13e567efa6d27fb80b62d243ef02e7e6
Dmitry Kuznetsov <121979741+uh-h@users.noreply.github.com>
2024-03-04 17:38:53 +0300

    added docstring, removed unnecessary commas

--- file_sync_client.py
+++ file_sync_client.py
@@ -1,4 +1,6 @@
 import asyncio
+from pathlib import Path
+
 import aiohttp
 import os
 from typing import Dict, List
@@ -10,19 +12,31 @@ import requests
 
 
 class FileSyncClient:
-    def __init__(self, sync_paths: List[str], mode: int = 0) -> None:
-        # mode == 0: soft mode, doesnt delete files from server
-        # mode == 1: hard mode, delete files from server if they are missing in client
+    def __init__(self, sync_paths: List[str | Path], mode: int = 0) -> None:
+        """
+        Class for managing file synchronization between the client and a server.
+
+        Args:
+            sync_paths (List[str]): A list of paths to directories to be synchronized.
+            mode (int, optional): Mode of synchronization.
+                - 0: Doesn't delete files form the server if they are not presented on local machine anymore (default).
+                - 1: Deletes files from the server if they are missing on local machine.
+        """
+        assert mode < 0 or mode > 1
         self.mode = mode
         self.sync_paths = sync_paths
         self.hash_table = {}
 
     async def start(self) -> None:
+        """
+        Starts the synchronization process, including fetching data from the server, indexing files on the client,
+        synchronizing client data with server data, and watching directories for change
+        """
         async with aiohttp.ClientSession() as session:
             tasks = [
                 # getting data from server
                 asyncio.create_task(requests.get_server_data(session)),
-                # indexing files on client
+                # indexing files on the client
                 asyncio.create_task(self.create_hash_table()),
             ]
 
@@ -30,7 +44,7 @@ class FileSyncClient:
             result = await asyncio.gather(*tasks)
             server_hash_table = result[0] if result[0] else {}
 
-            # waiting for client to be synchronized with server
+            # waiting for the client to be synchronized with server
             await self.sync_data(session, server_hash_table)
 
             # start watching directories and files for changes
@@ -39,6 +53,14 @@ class FileSyncClient:
             await handler.start()
 
     async def sync_data(self, session: aiohttp.ClientSession, server_hash_table: Dict) -> None:
+        """
+        Synchronizes client data with server data according to mode of synchronization
+
+        Args:
+            session (aiohttp.ClientSession): An aiohttp.ClientSession instance for making requests
+            server_hash_table (Dict): A Dict representing the server hash table mapping files to their respective hash
+                values
+        """
         logger.debug("Starting file sync...")
         tasks = []
 
@@ -76,6 +98,9 @@ class FileSyncClient:
         logger.debug("File sync with server complete")
 
     async def create_hash_table(self) -> None:
+        """
+        Creates a hash table mapping file paths to their respective hash values
+        """
         logger.debug("Creating hash...")
         for directory in self.sync_paths:
             for root, dirs, files in os.walk(directory):
@@ -87,11 +112,21 @@ class FileSyncClient:
         logger.debug(f"Hash table created!")
 
 
-def get_offline_event_type(file: str, file_hash: str, server_hash_table: Dict = None) -> int | str:
-    # returns old file name if name was changed, otherwise int
-    if server_hash_table is None:
-        server_hash_table = {}
+def get_offline_event_type(file: str | Path, file_hash: str, server_hash_table: Dict) -> int | str:
+    """
+    Determines the type of offline event for a given file based on its value and the server's hash table
 
+    Args:
+        file (str | Path): The file path that triggered the offline event
+        file_hash (str | Path): The hash value of that file
+        server_hash_table (Dict): A Dict representing the server's hash table
+
+    Returns:
+        int | str: The event type
+            - 1: File name was changed (content remains unchanged)
+            - 2: File content was changed
+            - str: Old file name if both the name and the content of the file was changed
+    """
     if file in server_hash_table and file_hash != server_hash_table[file]:
         # File content was changed
         return 2
